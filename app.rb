3a3874a27903b4ff0efa079606d6675318f28f63
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2024-03-12 18:02:41 +0000

    adds some business logic

--- app.rb
+++ app.rb
@@ -1,22 +1,42 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
 get("/") do
   erb(:index)
 
 end
 
 get("/rock") do
+  choices = ["rock", "paper", "scissors"]
+  @outcome = ""
+  @choice = choices[rand(0..2)]
+  if @choice == "rock"
+    @outcome = "tie"
+  end
   erb(:rock)
 
 end
 
 get("/paper") do
+  choices = ["rock", "paper", "scissors"]
+  @outcome = ""
+  @choice = choices[rand(0..2)]
+  if @choice == "paper"
+    @outcome = "tie"
+  end
   erb(:paper)
 
 end
 
 get("/scissors") do
+  choices = ["rock", "paper", "scissors"]
+  @outcome = ""
+  @choice == choices[rand(0..2)]
+  if @choice = "scissors"
+    @outcome = "tie"
+  end
+
   erb(:scissors)
 
 end
